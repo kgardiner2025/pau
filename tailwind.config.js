6b175a18422fa062a2fa4aff8bc364a0f2d46457
kgardiner2025 <kgardiner@paloaltou.edu>
2025-06-03 19:19:24 -0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Lexend', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
